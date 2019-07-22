e08c792ee4164806d7182dcb64371e7fe4a60057
dropdeadfu <dropdead@hashcat.nety>
2019-07-22 20:08:48 +0000

    buildscript will put libwkhtmltox.so into lib

--- lib/build-wkhtmltopdf.sh
+++ lib/build-wkhtmltopdf.sh
@@ -2,6 +2,8 @@
 
 # needs to be eecuted outside docker
 
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
 mkdir -p /tmp/wkhtmltopdf
 cd /tmp/wkhtmltopdf
 
@@ -12,7 +14,7 @@ cd packaging
 ./build compile-docker bionic-amd64 ../wkhtmltopdf/ bla
 
 # publish lib -> htmltopdf-java/src/main/resources/wkhtmltox/0.12.5/libwkhtmltox.so
-cp bla/wkhtmltox/lib/libwkhtmltox.so /tmp/libwkhtmltox.so
+cp bla/wkhtmltox/lib/libwkhtmltox.so $SCRIPTPATH/libwkhtmltox.so
 
 rm -rf /tmp/wkhtmltopdf
 
